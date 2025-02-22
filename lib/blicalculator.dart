b0672db840e1a9fadc99cab25b993a56b41cb568
rafiafsan <rafiafs45@gmail.com>
2025-02-22 22:51:44 +0600

    done

--- lib/blicalculator.dart
+++ lib/blicalculator.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class BMI extends StatefulWidget {
   const BMI({super.key});
 
-  
   @override
   State<BMI> createState() => _BMIState();
 }
@@ -20,47 +19,110 @@ class _BMIState extends State<BMI> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+        title: Text(
+          'BMI Calculator',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             TextFormField(
-              controller:ageController,
-              decoration: InputDecoration(
-                hintText: 'Age'
-              ),
+              controller: ageController,
+              decoration: InputDecoration(hintText: 'Age'),
             ),
             TextFormField(
-              controller:feetController,
-              decoration: InputDecoration(
-                  hintText: 'Height(Feet)'
-              ),
+              controller: feetController,
+              decoration: InputDecoration(hintText: 'Height(Feet)'),
             ),
             TextFormField(
-              controller:inchController,
-              decoration: InputDecoration(
-                  hintText: 'Height(Inch)'
-              ),
+              controller: inchController,
+              decoration: InputDecoration(hintText: 'Height(Inch)'),
             ),
             TextFormField(
-              controller:weightController,
-              decoration: InputDecoration(
-                  hintText: 'Weight(kg)'
-              ),
+              controller: weightController,
+              decoration: InputDecoration(hintText: 'Weight(kg)'),
             ),
             SizedBox(
               height: 20,
             ),
-            ElevatedButton(onPressed: (){
-              calculateBMI();
-            }, child: Text("Calculate")),
+            ElevatedButton(
+                onPressed: () {
+                  calculateBMI();
+                },
+                child: Text(
+                  "Calculate",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )),
             SizedBox(
               height: 10,
             ),
-            Text(bmi.toString())
-
+            Text(
+              bmi.toString(),
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 250,
+              width: 350,
+              child: SfRadialGauge(
+                  title: GaugeTitle(
+                      text: 'Your BMI Level',
+                      textStyle: const TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold)),
+                  axes: <RadialAxis>[
+                    RadialAxis(minimum: 16, maximum: 41, ranges: <GaugeRange>[
+                      GaugeRange(
+                          startValue: 16,
+                          endValue: 21,
+                          color: Colors.green,
+                          startWidth: 10,
+                          endWidth: 10),
+                      GaugeRange(
+                          startValue: 21,
+                          endValue: 25,
+                          color: Colors.orange,
+                          startWidth: 10,
+                          endWidth: 10),
+                      GaugeRange(
+                          startValue: 25,
+                          endValue: 31,
+                          color: Colors.yellow,
+                          startWidth: 10,
+                          endWidth: 10),
+                      GaugeRange(
+                          startValue: 31,
+                          endValue: 41,
+                          color: Colors.red,
+                          startWidth: 10,
+                          endWidth: 10)
+                    ], pointers: <GaugePointer>[
+                      NeedlePointer(value: bmi)
+                    ], annotations: <GaugeAnnotation>[
+                      GaugeAnnotation(
+                          widget: Container(
+                              child: Text("${bmi.toString()}",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold))),
+                          angle: 90,
+                          positionFactor: 0.5)
+                    ])
+                  ]),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Normal: 17-25\nFat: 25-31\nOverweight: 31-40",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
@@ -89,7 +151,6 @@ class _BMIState extends State<BMI> {
     }
   }
 
-
 // calculateBMI() {
   //   double feet = double.tryParse(feetController.text) ?? 0;
   //   double inch = double.tryParse(inchController.text) ?? 0;
@@ -104,6 +165,4 @@ class _BMIState extends State<BMI> {
   //
   //   });
   // }
-
-
 }
